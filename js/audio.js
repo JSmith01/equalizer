056ed4e0736866e38ff903aff2870f4130050076
Dmitry Zlygin <dmitry.zlygin@nordigy.ru>
2018-11-14 08:30:07 +0300

    refactored the code

--- js/audio.js
+++ js/audio.js
@@ -1,47 +1,75 @@
 function log(message) {
-	var out = document.getElementById('output');
-	var msg = document.createElement('div');
-	msg.textContent = message;
-	out.appendChild(msg);
+    var out = document.getElementById('output');
+    var msg = document.createElement('div');
+    msg.textContent = message;
+    out.appendChild(msg);
 }
 
-window.AudioContext = window.AudioContext||window.webkitAudioContext;
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
-window.onload = function() {
-	log('Hello there!');	
-    var c = document.getElementById('c');
-    var canvasContext = c.getContext('2d');
-    var audioContext = new AudioContext();
-    var a = document.getElementById('a');
-    a.crossOrigin = "anonymous";
-    var src = audioContext.createMediaElementSource(a);
-    var an = audioContext.createAnalyser();
-    an.fftSize = 2048;
-    src.connect(an);
-    src.connect(audioContext.destination);
-    var fqData = new Uint8Array(an.frequencyBinCount);
-    log('Frequency bin count: ' + an.frequencyBinCount);
-    
+function setupCanvas(canvasContext) {
     canvasContext.fillStyle = "green";
     canvasContext.lineWidth = 1;
-    var gradient = canvasContext.createLinearGradient(0,0,1,256);
+    var gradient = canvasContext.createLinearGradient(0, 0, 1, 256);
     gradient.addColorStop(0, "red");
     gradient.addColorStop(0.35, "orange");
     gradient.addColorStop(0.5, "green");
     gradient.addColorStop(0.65, "orange");
     gradient.addColorStop(1, "red");
     canvasContext.strokeStyle = gradient;
+}
+
+function getAnalyser(audioContext) {
+    var analyser = audioContext.createAnalyser();
+    analyser.fftSize = 2048;
+    log('Frequency bin count: ' + analyser.frequencyBinCount);
+
+    return analyser;
+}
+
+function getAudioSource(audioContext) {
+    var a = document.getElementById('a');
+    a.crossOrigin = "anonymous";
+
+    return audioContext.createMediaElementSource(a);
+}
+
+function getGeneratedSource(audioContext) {
+    var a = document.getElementById('a');
+    a.crossOrigin = "anonymous";
+
+    return audioContext.createMediaElementSource(a);
+}
+
+function drawEqualizer(canvasContext, width, height, fqData) {
+    canvasContext.clearRect(0, 0, width, height);
+    for (var i = 0; i < fqData.length; i++) {
+        //c.fillRect(i, (255-fqData[i])/2, 1, fqData[i]);
+        canvasContext.strokeRect(i, (255 - fqData[i]) / 2, 1, fqData[i]);
+    }
+}
+
+window.onload = function () {
+    log('Hello there!');
+    var c = document.getElementById('c');
+    var canvasContext = c.getContext('2d');
+    setupCanvas(canvasContext);
+
+    var audioContext = new AudioContext();
+
+    var an = getAnalyser(audioContext);
+    var fqData = new Uint8Array(an.frequencyBinCount);
+
+    var src = getAudioSource(audioContext);
+
+    src.connect(an);
+    src.connect(audioContext.destination);
 
     function renderFrame() {
         requestAnimationFrame(renderFrame);
         an.getByteFrequencyData(fqData);
-        canvasContext.clearRect(0, 0, c.width, c.height);
-        for (var i = 0; i < fqData.length ; i++) {
-            //c.fillRect(i, (255-fqData[i])/2, 1, fqData[i]);
-            canvasContext.strokeRect(i, (255-fqData[i])/2, 1, fqData[i]);
-        }
+        drawEqualizer(canvasContext, c.width, c.height, fqData);
     }
 
-    a.play();
     renderFrame();
 };
